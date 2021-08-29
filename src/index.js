dc202d2328f7fbb2084521d744650b5cb3cb5377
YaryhaAD <YaryhaAD@gmail.com>
2021-08-29 17:45:31 +0300

    feat: create functuion

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let pairs = bracketsConfig.map(i => i.join(''));
+  let result;
+  pairs.map(i => (result === undefined) ? result = str.split(i).join('') : result = result.split(i).join(''));
+  if (str !== result) {
+    return check(result, bracketsConfig);
+  } else if (str.length == 0) {
+    return true;
+  } else if (str.length != 0) {
+    return false;
+  }
 }
